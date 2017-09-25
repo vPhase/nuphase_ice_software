d7b03202cd92f11258bc60879b928b7cd00efccf
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2017-09-25 19:52:06 +0000

    add set attenuation command

--- src/nuphase-startup.c
+++ src/nuphase-startup.c
@@ -1,8 +1,12 @@
-
-/* Startup program.
+/* 
+ * \file nuphase-startup.c
  *
- * just runs at startup, reads temperatures, and exits when other things can start 
+ * Startup program. This makes sure the FPGA's dont' turn on
+ * until it's warm enough and that the heater is running properly. 
  *
+ * In the future, it may do an attenuation scan as well. 
+ *
+ * Right now this doesn't save any output (although I suppose things will be logged)
  *
  **/ 
 
@@ -11,55 +15,112 @@
 #include "nuphase-cfg.h" 
 #include "nuphase-common.h" 
 #include <stdio.h> 
+#include <string.h> 
+
 
 
 int main (int nargs, char ** args) 
 {
-
-  nuphase_hk_settings_t hkset;
-  nuphase_hk_init(&hkset); 
-
-
 
   //read the config file
   nuphase_start_cfg_t cfg; 
   nuphase_start_config_init(&cfg); 
 
   char * config_file = 0; 
+
   if (!nuphase_get_cfg_file(&config_file, NUPHASE_STARTUP))
   {
+    printf("Using config file: %s\n", config_file); 
     nuphase_start_config_read(config_file, &cfg); 
   }
+  
 
 
-
+  //turn off all fpga gpio's 
+  nuphase_set_gpio_power_state(0, GPIO_FPGA_ALL); 
 
   //now we can turn on the FPGA's via the ASPS so we have access to their temperature sensors
-  nuphase_set_asps_power_state (ASPS_ALL, cfg.asps_method); //should we just do HTTP since it's safer? 
+  nuphase_set_asps_power_state ( ASPS_MINIMAL|ASPS_FPGAS, cfg.asps_method); 
+
+  //wait a little bit
+  sleep(1); 
+
 
   nuphase_hk_t hk; 
+  int master_ok = 0; 
+  int slave_ok = 0; 
   // now loop until the temperature is warm enough 
-  while (1) 
+  while (!master_ok || !slave_ok) 
   {
+
+    //get the hk info 
     nuphase_hk(&hk, cfg.asps_method); 
-    int heater_current = nuphase_get_heater_current(cfg.asps_method); 
-    printf("master: %d slave: %d, case: %d, heater current: %d ", hk.temp_master, hk.temp_slave, hk.temp_case, heater_current); 
-    if (hk.temp_master > cfg.min_temperature && hk.temp_slave > cfg.min_temperature) 
+
+    nuphase_hk_print(stdout,&hk); 
+
+
+    if (hk.temp_master > cfg.min_temperature)
     {
-      //make sure we turn off heater. 
-      nuphase_set_heater_current(0, cfg.asps_method); 
-      break; 
+      printf("Master OK\n"); 
+      master_ok =1; 
     }
 
-    if (heater_current != cfg.heater_current) 
+    if (hk.temp_slave > cfg.min_temperature)
     {
-        nuphase_set_heater_current(cfg.heater_current, cfg.asps_method); 
+      printf("Slave OK, turning on\n"); 
+      slave_ok =1; 
+      nuphase_set_gpio_power_state( NP_FPGA_POWER_SLAVE, NP_FPGA_POWER_SLAVE); 
+      sleep(1); 
     }
+
+    if (master_ok && slave_ok) break; 
+
+
+    //make sure that the asps heater is on 
+    if (hk.asps_heater_current != cfg.heater_current) 
+    {
+        nuphase_set_asps_heater_current(cfg.heater_current, cfg.asps_method); 
+    }
+
+    //make sure that the aux heater is on
+    nuphase_set_gpio_power_state(NP_AUX_HEATER, NP_AUX_HEATER); 
  
     sleep(cfg.poll_interval); 
   } 
 
+  //turn off heaters
+  printf("Turning off heaters\n"); 
+  nuphase_set_asps_heater_current(0, cfg.asps_method); 
+  nuphase_set_gpio_power_state(0, NP_AUX_HEATER); 
 
+  //wait a bit 
+  usleep(100000); 
+
+  printf("Turning everything on\n"); 
+  //turn on the frontends? 
+  nuphase_set_asps_power_state(ASPS_ALL, cfg.asps_method); 
+
+  //turn on the master 
+  nuphase_set_gpio_power_state( NP_FPGA_POWER_MASTER, NP_FPGA_POWER_MASTER); 
+
+  //turn on the downhole??? 
+  // (not yet) 
+
+
+  usleep(100000); 
+  printf("Final state: \n"); 
+
+  nuphase_hk(&hk, cfg.asps_method); 
+  nuphase_hk_print(stdout,&hk); 
+
+
+  if (strlen(cfg.set_attenuation_cmd))
+  {
+    char cmd[1024]; 
+    sprintf(cmd,"%s %g %g", cfg.set_attenuation_cmd, cfg.desired_rms_master, cfg.desired_rms_slave); 
+    printf("Running: %s\n", cmd); 
+    system(cmd); 
+  }
 
 
 
